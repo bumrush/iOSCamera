19cda130d745bae8c3182db252d0d92218e8aec8
Justin Cano <jcano001@ucr.edu>
2015-05-01 23:50:07 -0700

    testing out AVFoundation capture session

--- Camera/CameraViewController.swift
+++ Camera/CameraViewController.swift
@@ -8,51 +8,71 @@
 
 import UIKit
 import MobileCoreServices
+import AVFoundation
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+class CameraViewController: UIViewController
 {
-    @IBAction func takePhoto(sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
-            let picker = UIImagePickerController()
-            picker.sourceType = .Camera
-            if let availableTypes = UIImagePickerController.availableMediaTypesForSourceType(.Camera) {
-                picker.mediaTypes = [kUTTypeImage]
-                picker.delegate = self
-                //picker.allowsEditing = true
-                presentViewController(picker, animated: true, completion: nil)
+    
+    @IBOutlet weak var capturedImage: UIImageView!
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func didPressTakePhoto(sender: UIButton) {
+        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    var dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, kCGRenderingIntentDefault)
+                    
+                    var image = UIImage(CGImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.Right)
+                    self.capturedImage.image = image
+                }
+            })
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input = AVCaptureDeviceInput(device: backCamera, error: &error)
+        
+        if error == nil && captureSession!.canAddInput(input) {
+            captureSession!.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            if captureSession!.canAddOutput(stillImageOutput) {
+                captureSession!.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.Portrait
+                previewView.layer.addSublayer(previewLayer)
+                
+                captureSession!.startRunning()
             }
-        } else {
-            println("camera not available!")
         }
     }
     
-    @IBAction func choosePhoto(sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
-            let picker = UIImagePickerController()
-            picker.sourceType = .PhotoLibrary
-            if let availableTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary) {
-                picker.mediaTypes = [kUTTypeImage]
-                picker.delegate = self
-                //picker.allowsEditing = true
-                presentViewController(picker, animated: true, completion: nil)
-            }
-        }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer!.frame = previewView.bounds
     }
     
-    // MARK: - Delegate Methods
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject] ) {
-        var image = info[UIImagePickerControllerEditedImage] as? UIImage
-        if image == nil {
-            image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        }
-        //process image
-        println("image picked!")
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-
 }
